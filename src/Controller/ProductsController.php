f9a4ce3fcab062d9962114c6941af14d92c82ba8
xuananpham93 <xuananpham93@gmail.com>
2016-11-05 17:34:49 +0700

    Update ProductsController.php

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -20,10 +20,10 @@ class ProductsController extends AppController
 		$product = $this->Products->get($id, [
 			'contain' => ['Brands']
 			]);
-		$this->Products->updateAll(array('Products.view'=> $product->view + 1), array('Products.id'=>$id));
+		$this->Products->updateAll(array('Products.view'=> $product->view + 1), array('id'=>$id));
 
 		$related = $this->Products->find()
-		->where(['Products.cat_id' => $product->cat_id, 'Products.id <>' => $product->id])
+		->where(['Products.cat_id' => $product->cat_id, 'id <>' => $product->id])
 		;
 
 		$this->loadModel('Brands');
